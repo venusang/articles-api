325852009fbccafdec6ac6447e34f029c5957112
Venus Ang <venusang24@gmail.com>
2020-01-23 18:50:09 -0800

    Adding source files

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+Article.create([
+  { title: "Article 1", body: "Lorem 1 ipsum" },
+  { title: "Article 2", body: "Lorem 2 ipsum" },
+  { title: "Article 3", body: "Lorem 3 ipsum" },
+  { title: "Article 4", body: "Lorem 4 ipsum" },
+  { title: "Article 5", body: "Lorem 5 ipsum" },
+  { title: "Article 6", body: "Lorem 6 ipsum" },
+  { title: "Article 7", body: "Lorem 7 ipsum" },
+  { title: "Article 8", body: "Lorem 8 ipsum" },
+  { title: "Article 9", body: "Lorem 9 ipsum" },
+  { title: "Article 10", body: "Lorem 10 ipsum" },
+  { title: "Article 11", body: "Lorem 11 ipsum" },
+  { title: "Article 12", body: "Lorem 12 ipsum" },
+  { title: "Article 13", body: "Lorem 13 ipsum" }
+])
